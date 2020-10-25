5d15f6a7e4df8b5b633c89acbf82d973a359c527
Christian Hockenberger <blaschwall@gmail.com>
2020-10-25 15:54:14 +0100

    Added shortcode to display featured image of recent article as a background image

--- framework/modules/marlon_shortcodes/marlon_shortcodes.php
+++ framework/modules/marlon_shortcodes/marlon_shortcodes.php
@@ -10,6 +10,7 @@ if ( ! class_exists( 'Marlon_Shortcodes' ) ) {
 		public function add_module_shortcodes() {
 			add_shortcode( 'marlon-recent-posts', array( $this, 'recent_posts' ) );
 			add_shortcode( 'marlon-recent-post', array( $this, 'recent_post' ) );
+			add_shortcode( 'marlon-recent-post-image', array( $this, 'recent_post_image' ) );
 		}
 
 		public function recent_posts( $atts = array() ) {
@@ -119,6 +120,51 @@ if ( ! class_exists( 'Marlon_Shortcodes' ) ) {
 								<h6 class="recent-post-category-date"><?php $utils->the_permalink_date( '', '<br>', false ); ?><?php the_category( ', ' ); ?></h6>
 							</div>
 						</div>
+
+						<div class="recent-post-image"><img src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>" alt="<?php the_title(); ?>"></div>
+
+					</div><!-- .recent-post-container -->
+					<?php endif; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+			<?php
+			return ob_get_clean();
+
+		}
+
+
+		public function recent_post_image( $atts = array() ) {
+
+			extract(
+				shortcode_atts(
+					array(
+						'category' => 'uncategorized',
+						'title'    => __( 'Recent Article', 'marlon' ),
+						'offset'   => 0,
+						'limit'    => 1
+					),
+					$atts
+				)
+			);
+
+			ob_start();
+			?>
+
+			<?php $the_query = new WP_Query( array( 'category_name' => $category, 'offset' => $offset, 'posts_per_page' => $limit ) ); ?>
+
+			<?php if ( $the_query->have_posts() ) : ?>
+				<?php while ( $the_query->have_posts() ) : ?>
+					<?php $the_query->the_post(); ?>
+					<?php if ( has_post_thumbnail() ) : ?>
+					<div class="recent-post-container">
+
+						<?php if ( $title ) : ?>
+						<div class="recent-post-header">
+							<h2 class="recent-post-header-title"><?php echo esc_html( $title ); ?></h2>
+						</div>
+						<?php endif; ?>
 
 						<div class="recent-post-image"><img src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>" alt="<?php the_title(); ?>"></div>
 
